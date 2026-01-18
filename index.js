749111362fdb5996c43d00e2b98e473ad5bea570
omarabukaraki <abukarakiomar@gmail.com>
2026-01-18 16:16:17 +0300

    fix

--- index.js
+++ index.js
@@ -3,39 +3,38 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
 
 let io;
 
-if (!io) {
-  io = new Server(server, {
-    cors: {
-      origin: "https://client-rho-sandy.vercel.app",
-      methods: ["GET", "POST"],
-    },
-  });
-
-  io.on("connection", (socket) => {
-    console.log("user connected:", socket.id);
-
-    socket.on("join_room", (room) => {
-      socket.join(room);
-    });
-
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("user disconnected");
-    });
-  });
-}
-
 module.exports = (req, res) => {
   if (!res.socket.server.io) {
+    io = new Server(res.socket.server, {
+      path: "/api/socket",
+      cors: {
+        origin: "https://client-rho-sandy.vercel.app",
+        methods: ["GET", "POST"],
+      },
+    });
+
     res.socket.server.io = io;
+
+    io.on("connection", (socket) => {
+      console.log("connected:", socket.id);
+
+      socket.on("join_room", (room) => {
+        socket.join(room);
+      });
+
+      socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("disconnected:", socket.id);
+      });
+    });
   }
+
   res.end();
 };
