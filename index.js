d1f9cf43b7ac090bd0f9cee82ba5a7ae020f91af
omarabukaraki <abukarakiomar@gmail.com>
2026-01-18 16:34:09 +0300

    add local

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://client-rho-sandy.vercel.app",
+    origin: ["https://client-rho-sandy.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
