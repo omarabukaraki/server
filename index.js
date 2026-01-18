d747fb88572279b1d725b553a33d12a59e2c79a7
omarabukaraki <abukarakiomar@gmail.com>
2026-01-18 16:04:41 +0300

    start

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const app = express();
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on *:3001");
+});
